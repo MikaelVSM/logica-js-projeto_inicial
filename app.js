1ef37c95e1358a6843f9bb52efed171ef4ed85e3
Mikael Morais <mikaelvitorsm123@gmail.com>
2025-01-23 20:35:47 -0300

    Inserção do else

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-alert("Boas vindas ao jogo do número secreto");
+alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
-let chute = prompt("Escolha um número entre 1 a 10");
+let chute = prompt('Escolha um número entre 1 a 10');
 
 if (numeroSecreto == chute) {
-    console.log("isso aí! Você descobriu o número secreto (5)");
+    console.log(`isso aí! Você descobriu o número secreto ${numeroSecreto}`);
 
+} else {
+    alert('Você errou :(')
 }
