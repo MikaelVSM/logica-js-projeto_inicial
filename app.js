457da049d091adea51a7f521f07e29f757be8bd6
Mikael Morais <mikaelvitorsm123@gmail.com>
2025-01-27 21:41:42 -0300

    Adição dos comandos console.log e comentários sobre as funcionalidade

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 9;
+
+//Console.log para verificar o valor de "chute" após a entrada do usuário
 let chute = prompt('Escolha um número entre 1 a 10');
+console.log('Valor do chute: ', chute);
+
+let numeroSecreto = 9;
+
+//Console.log para verificar a comparação entre "chute" e "numeroSecreto"
+console.log('Resultado da comparação:', chute == numeroSecreto);
 
 if (numeroSecreto == chute) {
-    console.log(`isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+   alert('Acertou!');
 
 } else {
     alert('Você errou! O número secreto era ' + numeroSecreto + ', mas foi digitado o número ' + chute)
