a3b92a80357c10137c644c778c561dee1efab118
Mikael Morais <mikaelvitorsm123@gmail.com>
2025-01-27 21:32:55 -0300

    Inserção de mais elementos

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 9;
 let chute = prompt('Escolha um número entre 1 a 10');
 
 if (numeroSecreto == chute) {
     console.log(`isso aí! Você descobriu o número secreto ${numeroSecreto}`);
 
 } else {
-    alert('Você errou :(')
+    alert('Você errou! O número secreto era ' + numeroSecreto + ', mas foi digitado o número ' + chute)
 }
